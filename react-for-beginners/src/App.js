a65acad75d42c7f6b696c8d878be96ab96fd447e
YunHak <sza0203@naver.com>
2022-01-17 20:18:40 +0900

    Clean Up

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,33 +1,21 @@
 import { useState, useEffect } from "react";
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeword] = useState("");
-  const onCilck = () => setValue((current) => current + 1);
-  const onChange = (event) => setKeword(event.target.value);
+
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]); // keyword 가 변화 할때 코드를 실행 watching
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((current) => !current);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onCilck}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
-
 export default App;
