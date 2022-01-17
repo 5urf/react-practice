b5a499e1af0632a195ebdcd9a2fef99f1d77aa8a
YunHak <sza0203@naver.com>
2022-01-17 18:45:02 +0900

    useEffect Deps

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,13 +1,29 @@
 import { useState, useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeword] = useState("");
   const onCilck = () => setValue((current) => current + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeword(event.target.value);
   useEffect(() => {
-    console.log("call the API...");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]); // keyword 가 변화 할때 코드를 실행 watching
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onCilck}>click me</button>
     </div>
