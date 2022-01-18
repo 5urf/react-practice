44d6387e0c7711864d888d55d1bccd5b1329fb10
LeeYunHak <sza0203@naver.com>
2022-01-18 17:33:38 +0900

    디테일 페이지 작업중

---
+++ react-for-beginners/src/components/DetailMovie.js
@@ -0,0 +1,8 @@
+function DetailMovie({ coverImg }) {
+  return (
+    <div>
+      <img src={coverImg} alt="gg" />
+    </div>
+  );
+}
+export default DetailMovie;
--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import DetailMovie from "../components/DetailMovie";
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <DetailMovie coverImg={movie.medium_cover_image} />
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
